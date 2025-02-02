96969869a67aea375a2d17c395365536ce8a4faf
Asher Etherington <asherce21@gmail.com>
2025-02-01 19:53:29 -0800

    a

--- src/Scenes/Craps.js
+++ src/Scenes/Craps.js
@@ -3,47 +3,59 @@ class Craps extends Phaser.Scene {
         super("crapsScene");
     }
 
+
+
     preload() {
     }
 
     init() {
+
         // variables and settings
         this.bank = 1000;
         this.bet = 5;
+        this.sum = 0;
+        this.dice1 = 0;
+        this.dice2 = 0;
+
+        this.bankText = this.add.text(250, 100, `Bank: $${this.bank}`, { fontSize: '32px', fill: '#FFF' });
+
+        this.roll = () => {
+            // Generate random dice rolls
+            this.dice1 = Phaser.Math.Between(1, 6);
+            this.dice2 = Phaser.Math.Between(1, 6);
+            this.sum = this.dice1 + this.dice2;
+        };
+
+        this.lose = () => {
+            this.bank -= this.bet;
+            this.bankText.setText(`Bank: $${this.bank}`);
+        }
+
+        this.win = () => {
+            this.bank += this.bet;
+            this.bankText.setText(`Bank: $${this.bank}`);
+        }
 
     }
 
     create() {
-            // Display dice images
-            this.dice1 = this.add.image(300, 250, 'dice1').setScale(0.5);
-            this.dice2 = this.add.image(500, 250, 'dice1').setScale(0.5);
-    
-            // Roll button
-            this.rollButton = this.add.image(400, 400, 'button').setInteractive();
-    
-            // Result text
-            this.resultText = this.add.text(250, 500, "Click to Roll!", { fontSize: '32px', fill: '#FFF' });
-    
-            // Add event listener to roll dice
-            this.rollButton.on('pointerdown', this.rollDice, this);
-        }
 
-    update() {
-         // Generate random dice rolls
-         let roll1 = Phaser.Math.Between(1, 6);
-         let roll2 = Phaser.Math.Between(1, 6);
-         let sum = roll1 + roll2;
- 
-         // Update dice images
-         this.dice1.setTexture(`dice${roll1}`);
-         this.dice2.setTexture(`dice${roll2}`);
- 
-         // Check if sum is 5
-         if (sum === 5) {
-             this.resultText.setText(`You rolled 5: Bonus Win!`);
-             this.bank += this.bet * 2; // Example: Winning doubles the bet
-         } else {
-             this.resultText.setText(`You rolled ${sum}. Try again!`);
-         }
+
+        // Result text
+        this.resultText = this.add.text(250, 500, "Press Enter to Roll!", { fontSize: '32px', fill: '#FFF' });
+        //make an event listener for the enter key
+        this.input.keyboard.on('keydown-ENTER', () => {
+            // Generate random dice rolls
+            this.roll();
+            // Check if sum is 5
+            if (this.sum === 7) {
+                this.resultText.setText(`You rolled 7: Bonus Win!`);
+                this.win();
+            } else {
+                this.resultText.setText(`You rolled ${this.sum}. Try again!`);
+                this.lose();
+            }
+        });
     }
+
 }
